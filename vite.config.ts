14c16d5114cfe5026226999339aeefdc33e4cf37
Victoria <nestea.wyatt@gmail.com>
2026-05-27 23:28:01 +0300

    chore(app): configure tauri vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -15,16 +15,12 @@ export default defineConfig(async () => ({
     },
   },
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent Vite from obscuring rust errors
   clearScreen: false,
 
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
-    host: host || false,
+    host: host || '127.0.0.1',
     hmr: host
       ? {
           protocol: 'ws',
@@ -33,7 +29,6 @@ export default defineConfig(async () => ({
         }
       : undefined,
     watch: {
-      // 3. tell Vite to ignore watching `src-tauri`
       ignored: ['**/src-tauri/**'],
     },
   },
