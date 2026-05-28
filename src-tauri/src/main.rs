c9c1ce59c7cc0a0a547012b097a479df3ba17d5d
Victoria <nestea.wyatt@gmail.com>
2026-05-28 06:08:35 +0300

    deps(clipboard): remove unused tauri clipboard plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,7 +11,6 @@ use commands::vault_commands::{create_vault, lock_vault, unlock_vault};
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_clipboard_manager::init())
         .manage(state::AppState::default())
         .invoke_handler(tauri::generate_handler![
             create_vault,
